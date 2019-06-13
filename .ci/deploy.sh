537f48f221de5892440db0242668d5a5afa996c1
Reece Dunham <me@rdil.rocks>
2019-06-13 11:42:38 -0400

    Set git user when deploying docs (#331)
    * Set git user when deploying docs
    
    * Fix wrong email

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
+git config --global user.name "Cirrus CI"
+git config --global user.name "hello@cirruslabs.org"
 git remote set-url origin https://$DEPLOY_TOKEN@github.com/cirruslabs/cirrus-ci-docs/
 mkdocs gh-deploy --force --remote-branch gh-pages
