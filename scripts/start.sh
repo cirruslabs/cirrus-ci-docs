df4a727ff10f2529319ef77a11c388caee9b9fef
Reece Dunham <me@rdil.rocks>
2019-06-17 08:40:37 -0400

    Bumpety bump

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -p 8000:8000 -v ${PWD}:/docs squidfunk/mkdocs-material:4.3.1
+docker run --rm -it -p 8000:8000 -v ${PWD}:/docs squidfunk/mkdocs-material:4.4.0
