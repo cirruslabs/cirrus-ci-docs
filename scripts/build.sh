338ab8b0c5a27640f8d574fe912fc192630e484c
Reece Dunham <me@rdil.rocks>
2019-10-03 17:22:00 -0400

    Update scripts/build.sh
    Co-Authored-By: Fedor Korotkov <fedor.korotkov@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material:{cat theme/.material-version.cfg} build
+docker run --rm -it -v ${PWD}:/docs squidfunk/mkdocs-material:$(grep 'MKDOCS_VERSION:' .cirrus.yml | cut -d\  -f4) build
