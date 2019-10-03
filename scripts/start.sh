63ca21bf3ad02d63b3c7f5a4e771b82946e089d8
Reece Dunham <me@rdil.rocks>
2019-10-03 16:07:06 -0400

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -p 8000:8000 -v ${PWD}:/docs squidfunk/mkdocs-material:4.4.2
+docker run --rm -it -p 8000:8000 -v ${PWD}:/docs squidfunk/mkdocs-material:{cat theme/.material-version.cfg}
