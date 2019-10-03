c6185878eeddcdfb7f43ef2edc96f2e00c1a4af1
Reece Dunham <me@rdil.rocks>
2019-10-03 17:22:06 -0400

    Update scripts/start.sh
    Co-Authored-By: Fedor Korotkov <fedor.korotkov@gmail.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -it -p 8000:8000 -v ${PWD}:/docs squidfunk/mkdocs-material:{cat theme/.material-version.cfg}
+docker run --rm -it -p 8000:8000 -v ${PWD}:/docs squidfunk/mkdocs-material:$(grep 'MKDOCS_VERSION:' .cirrus.yml | cut -d\  -f4)
