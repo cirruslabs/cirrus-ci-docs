3283e7781f2e6e0df4ab49e4255b83e7ed8d27eb
fkorotkov <fedor.korotkov@gmail.com>
2020-12-01 09:08:16 -0500

    --verbose deploy

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -3,4 +3,4 @@
 git config --global user.name "Cirrus CI"
 git config --global user.name "hello@cirruslabs.org"
 git remote set-url origin https://$DEPLOY_TOKEN@github.com/cirruslabs/cirrus-ci-docs/
-mkdocs gh-deploy --force --remote-branch gh-pages
+mkdocs --verbose gh-deploy --force --remote-branch gh-pages
